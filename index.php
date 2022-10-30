94e41d0afa5ac5f269f0a8f384ca1b05774f1523
NOTMEE12 <aleks.baran01@gmail.com>
2022-10-30 14:20:19 +0100

    testing 2

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
     include_once("home.html");
-    echo "<script> normal_text = 'try ' </script>"
+    $normal_text = 'Tu nie ma żadnej wiadomości! napisz swoją, która będzie wyświetlona przez 8 godzin!';
+    $your_text = null;
+
+
+    function update_text() {
+        echo "check_update()";
+        $your_text = "<script> document.write(your_text) </script>"
+        echo "<script> normal_text = ${your_text} </script>";
+    }
 ?>
