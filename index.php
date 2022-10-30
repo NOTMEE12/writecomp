198207e211f7b716bf2d27d834a3c85a658e93f1
NOTMEE12 <aleks.baran01@gmail.com>
2022-10-30 19:53:32 +0100

    trying make public with php

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
     include_once("home.html");
-    $normal_text = 'Tu nie ma żadnej wiadomości! napisz swoją, która będzie wyświetlona przez 8 godzin!';
-    $your_text = null;
-    $time_end = 0;
-    
-    function update_all_clients() {
-        echo "<script> your_text = ${your_text} </script>";
-        echo "<script> time_end = new Date(${time_end}) </script>";
-        $your_text = text;
-        $time_end = Time;
-    }
 ?>
