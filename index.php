910da59155c585fe8e0dcd4bbce2a22def364910
NOTMEE12 <aleks.baran01@gmail.com>
2022-10-30 14:39:35 +0100

    test 4

--- index.php
+++ index.php
@@ -2,4 +2,9 @@
     include_once("home.html");
     $normal_text = 'Tu nie ma żadnej wiadomości! napisz swoją, która będzie wyświetlona przez 8 godzin!';
     $your_text = null;
+
+    function update_all_clients(text, time) {
+        echo "<script> your_text = ${text} </script>";
+        echo "<script> time_end = new Date(${time}) </script>"
+    }
 ?>
