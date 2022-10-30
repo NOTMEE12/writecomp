59fdc81359f6683edaa4ecbcd7ecc2d1bc4fbf59
NOTMEE12 <aleks.baran01@gmail.com>
2022-10-30 19:25:28 +0100

    trying make public with php

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
     $your_text = null;
     $time_end = 0;
     
-    function update_all_clients() {
-        echo "<script> your_text = ${your_text} </script>";
+    function update_all_clients(text, Time) {
+        echo "<script> your_text = ${text} </script>";
         echo "<script> time_end = new Date(${Time}) </script>";
+        $your_text = text;
+        $time_end = Time;
     }
 ?>
