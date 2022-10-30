5445b0b584453d27929ddd8bb7f74d4fc17d53e0
NOTMEE12 <aleks.baran01@gmail.com>
2022-10-30 18:43:52 +0100

    trying make public

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
     $normal_text = 'Tu nie ma żadnej wiadomości! napisz swoją, która będzie wyświetlona przez 8 godzin!';
     $your_text = null;
 
-    function update_all_clients(text, time) {
+    
+    function update_all_clients(text, Time) {
         echo "<script> your_text = ${text} </script>";
-        echo "<script> time_end = new Date(${time}) </script>"
+        echo "<script> time_end = new Date(${Time}) </script>"
     }
 ?>
