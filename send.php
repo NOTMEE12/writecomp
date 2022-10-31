bfa594166d4a405d7cb4b386fe4bd02b7e0a1498
NOTMEE12 <aleks.baran01@gmail.com>
2022-10-31 21:11:07 +0100

    maybe solve

--- send.php
+++ send.php
@@ -7,7 +7,7 @@
 	# RETURN VALUE
     $last = file_get_contents($file_name);
     print_r("last word: $last\n");
-    file_put_contents($file_name, "$text; $end_time");
+    file_put_contents($file_name, "$text; $end_time;");
     print_r("from PHP: $text; $end_time\n");
-    print_r("from PHP Post: $_POST;\n");
+    print_r("from PHP Post: {$json_deocode('$_POST')}};\n");
 ?>
