92cabdcefe429295b42f603101ee32108eedf1dc
NOTMEE12 <aleks.baran01@gmail.com>
2022-10-31 16:15:32 +0100

    PHP

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ include_once("home.html");
 
 
 $text = $_POST['TEXT'];
-$end_time = $_POST['ENDTIME']
+$end_time = $_POST['ENDTIME'];
     
 if (text != null and $end_time != null) {
-    echo "<script> console.log('from php: ${text}; ${end_time}')"
+    echo "<script> console.log('from php: ${text}; ${end_time}')";
 }
 ?>
