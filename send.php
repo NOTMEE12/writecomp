edc1795749f771e57009f63710f3c86f00b3efea
NOTMEE12 <aleks.baran01@gmail.com>
2022-10-31 18:54:03 +0100

    maybe solve

--- send.php
+++ send.php
@@ -1,4 +1,5 @@
 <?php
+	$file_name = "key.txt";
     $text = $_POST['Text'];
     $end_time = $_POST['Endtime'];
 
@@ -8,5 +9,8 @@
     if ($end_time == null) {
     	$end_time = "undefined";
     }
+    $last = file_get_contents($file_name);
+    echo("last word: $last");
+    file_put_contents($file_name, "$text; $end_time");
     echo("from PHP Post: $text; $end_time");
 ?>
