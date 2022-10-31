ab8171210df1458c5959f2ae8157a9f4fb6369ac
NOTMEE12 <aleks.baran01@gmail.com>
2022-10-31 16:40:39 +0100

    PHP

--- send.php
+++ send.php
@@ -3,6 +3,6 @@ $text = $_POST['TEXT'];
 $end_time = $_POST['ENDTIME'];
     
 if ($text != null) {
-    echo "<script> console.log('from php: ${text}; ${end_time}')";
+    echo "<script> console.log('from php: ". $text .";". $end_time ."')";
 }
 ?>
