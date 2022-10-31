c7465f95657750a5d03e4cb3824f2520e5dc9dcf
NOTMEE12 <aleks.baran01@gmail.com>
2022-10-31 20:48:31 +0100

    maybe solve

--- send.php
+++ send.php
@@ -1,13 +1,13 @@
 <?php
 	# GET VALUE
 	$file_name = "key.txt";
-    $text = $_POST['Text'];
-    $end_time = $_POST['Endtime'];
+    $text = json_decode($_POST['Text']);
+    $end_time = json_decode($_POST['Endtime']);
 
 	# RETURN VALUE
     $last = file_get_contents($file_name);
     print_r("last word: $last\n");
     file_put_contents($file_name, "$text; $end_time");
-    print_r("from PHP: $text; $end_time");
+    print_r("from PHP: $text; $end_time\n");
     print_r("from PHP Post: $POST");
 ?>
