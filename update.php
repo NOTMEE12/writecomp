567d43ec615203bd09c2528c4b81d49cd36a1d0f
NOTMEE12 <aleks.baran01@gmail.com>
2022-11-01 14:33:14 +0100

    maybe solve

--- update.php
+++ update.php
@@ -1,9 +1,4 @@
 <?php
 	$text = file_get_contents("key.txt");
-	$time = explode(" | ", $text)[1];
-	$miliseconds = floor(microtime(true) * 1000);
-	$time = $time - $miliseconds;
-	$time = " | ". $time;
-	$text .= $time;
 	print_r($text)
 ?>
